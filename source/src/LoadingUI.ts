886e532f91d60796ac4487008c099089c4a02696
zhishaofei <zhishaofei@jd.com>
2015-07-19 12:13:35 +0800

    格式整理

--- source/src/LoadingUI.ts
+++ source/src/LoadingUI.ts
@@ -26,7 +26,6 @@
  */
 
 class LoadingUI extends egret.Sprite {
-
 	public constructor() {
 		super();
 		this.createView();
@@ -36,11 +35,11 @@ class LoadingUI extends egret.Sprite {
 
 	private createView():void {
 		this.textField = new egret.TextField();
-		this.addChild(this.textField);
 		this.textField.y = 300;
 		this.textField.width = 480;
 		this.textField.height = 100;
 		this.textField.textAlign = "center";
+		this.addChild(this.textField);
 	}
 
 	public setProgress(current, total):void {
